a7354e2d8fcd12919fe35a51b604eca3eba837e1
Adrien Carpentier <me@adriencarpentier.com>
2024-04-07 16:17:07 +0200

    feat: use vite and POC of live microphone sampling

--- index.js
+++
@@ -1,9 +0,0 @@
-import { analyze } from 'web-audio-beat-detector';
-
-analyze(audioBuffer)
-    .then((tempo) => {
-        // the tempo could be analyzed
-    })
-    .catch((err) => {
-        // something went wrong
-    });
